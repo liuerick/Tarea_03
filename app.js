2bd2c5b664b3515075e6ce2c6a8ee16ab1324f0b
liuerick <liuerick6@gmail.com>
2026-06-19 12:59:43 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
+ <style>
+    #vis.vega-embed {
+      width: 100%;
+      display: flex;
+    }
 
+    #vis.vega-embed details,
+    #vis.vega-embed details summary {
+      position: relative;
+    }
+  </style>
